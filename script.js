7360b8bee7a0c20bdd8e1d032c4b9e21276194ee
Ibrahim Halil <125586449+ibrahimhalilsezgin@users.noreply.github.com>
2024-06-15 02:06:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var timestamp = 1194644981;
+
+function updateAge() {
+    var currentTime = Date.now();
+
+    var startDate = new Date(timestamp * 1000);
+    var timeDifference = currentTime - startDate.getTime();
+    var ageInYears = timeDifference / (1000 * 60 * 60 * 24 * 365.25);
+    document.getElementById('old').textContent = `${ageInYears.toFixed(8)} y.o`;
+}
+
+setInterval(updateAge, 0);
+
+updateAge();
